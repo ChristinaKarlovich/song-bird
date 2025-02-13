7de82ad5a58e93231e0ec224662dafe01335226b
ChristinaKarlovich <christina.karlovich@gmail.com>
2025-02-13 22:08:01 +0300

    feat: Add catalog styles. Add modal window with bird info

--- src/pages/catalog/index.js
+++ src/pages/catalog/index.js
@@ -1,33 +1,83 @@
+import './style.scss';
 import birdsdata from '../../assets/birdsdata';
 
-var birdsList = birdsdata.flat();
+let divWrapper = null;
 
-var itemUl = document.getElementById('catalog-list');
-birdsList
-  .sort((a, b) => {
-    if (a.name > b.name) return 1;
-    else if (a.name < b.name) return -1;
-    else return 0;
-  })
-  .forEach((element) => {
-    var itemLi = document.createElement('li');
-    itemUl.appendChild(itemLi);
+setCatalog();
 
-    var divContent = document.createElement('div');
-    var divName = document.createElement('div');
-    divName.innerText = element.name;
-    var divSpecies = document.createElement('div');
-    divSpecies.innerText = element.species;
-    var imgBird = document.createElement('img');
-    imgBird.src = element.image;
+function closeCatalogItemInfo(event) {
+  if (event.target != null && event.target === divWrapper) {
+    document.body.removeChild(divWrapper);
+  }
+}
 
-    var audioSong = document.createElement('audio');
-    audioSong.src = element.audio;
-    audioSong.controls = true;
-
-    var pDescription = document.createElement('p');
-    pDescription.innerText = element.description;
-
-    divContent.append(divName, divSpecies, imgBird, audioSong, pDescription);
-    itemLi.appendChild(divContent);
+function showCatalogItemInfo(element) {
+  divWrapper = document.createElement('div');
+  divWrapper.id = 'element-info-wrapper';
+  divWrapper.classList.add('element-info-wrapper');
+  divWrapper.addEventListener('click', (event) => {
+    closeCatalogItemInfo(event);
   });
+  document.body.append(divWrapper);
+
+  let divInfo = document.createElement('div');
+  divInfo.classList.add('element-info');
+  divWrapper.append(divInfo);
+
+  let imgBird = document.createElement('img');
+  imgBird.classList.add('element-image');
+  imgBird.src = element.image;
+  divInfo.append(imgBird);
+
+  let pName = document.createElement('div');
+  pName.classList.add('element-name');
+  pName.innerText = element.name;
+  divInfo.append(pName);
+
+  let pSpecies = document.createElement('div');
+  pSpecies.classList.add('element-species');
+  pSpecies.innerHTML = element.species;
+  divInfo.append(pSpecies);
+
+  let pDescription = document.createElement('div');
+  pDescription.classList.add('element-description');
+  pDescription.innerHTML = element.description;
+  divInfo.append(pDescription);
+
+  let audioSong = document.createElement('audio');
+  audioSong.classList.add('element-audio');
+  audioSong.src = element.audio;
+  audioSong.controls = true;
+  divInfo.append(audioSong);
+}
+
+function setCatalog() {
+  var birdsList = birdsdata.flat();
+
+  var itemUl = document.getElementById('catalog-list');
+  birdsList
+    .sort((a, b) => {
+      if (a.name > b.name) return 1;
+      else if (a.name < b.name) return -1;
+      else return 0;
+    })
+    .forEach((element) => {
+      var itemLi = document.createElement('li');
+      itemLi.classList.add('catalog-item');
+      itemLi.addEventListener('click', () => {
+        showCatalogItemInfo(element);
+      });
+      itemUl.appendChild(itemLi);
+
+      var divContent = document.createElement('div');
+      divContent.classList.add('catalog-item-content');
+      divContent.style.backgroundImage = `url(${element.image})`;
+
+      var divName = document.createElement('div');
+      divName.classList.add('catalog-item-name');
+      divName.innerText = element.name;
+
+      divContent.append(divName);
+      itemLi.appendChild(divContent);
+    });
+}
