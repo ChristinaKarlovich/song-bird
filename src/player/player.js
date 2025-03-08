1dcdd5b3f9799ef89c6652a806b7b1a54aced302
ChristinaKarlovich <christina.karlovich@gmail.com>
2025-03-08 21:34:41 +0300

    feat: change volume and volume img by clicking volume btn

--- src/player/player.js
+++ src/player/player.js
@@ -1,5 +1,7 @@
-import playSVG from '../assets/icon/play-button.svg';
-import pauseSVG from '../assets/icon/pause-button.svg';
+import playPNG from '../assets/icon/play.png';
+import pausePNG from '../assets/icon/pause.png';
+import volumePNG from '../assets/icon/volume.png';
+import mutePNG from '../assets/icon/mute.png';
 import './style.scss';
 
 let playBtn = null;
@@ -9,6 +11,7 @@ let divCurrentTime = null;
 let rangeDuration = null;
 let songDurationFormat = 0;
 let rangeVolume = null;
+let volumeBtnIcon = null;
 
 export function drawPlayer(container, audio) {
   let playerWrapper = document.createElement('div');
@@ -21,36 +24,39 @@ export function drawPlayer(container, audio) {
   audioSong.addEventListener('timeupdate', fillCurrentTime);
   audioSong.addEventListener('ended', () => {
     playBtn.classList.toggle('play-mode');
+    audioSong.currentTime = 0;
     setBtnIcon();
   });
   audioSong.addEventListener('canplaythrough', () => {
     let songDuration = getTimePartsBySeconds(audioSong.duration);
     songDurationFormat = formateTime(songDuration);
+    audioSong.volume = 0.5;
+    rangeVolume.value = audioSong.volume * 10;
+    rangeVolume.style.background =
+      'linear-gradient(to right,#ffffff ' +
+      rangeVolume.value * 10 +
+      '%,#ffffff80 ' +
+      rangeVolume.value * 10 +
+      '%)';
     fillCurrentTime();
   });
 
   function fillCurrentTime() {
     let songTime = getTimePartsBySeconds(audioSong.currentTime);
     setCurrentDuration(audioSong.currentTime);
-    divCurrentTime.innerText = `${formateTime(songTime)}/${songDurationFormat}`;
+    divCurrentTime.innerText = `${formateTime(
+      songTime,
+    )} / ${songDurationFormat}`;
   }
 
   container.append(audioSong);
 
-  divCurrentTime = document.createElement('div');
-  playerWrapper.append(divCurrentTime);
+  let divAudioControlsWrapper = document.createElement('div');
+  divAudioControlsWrapper.classList.add('audio-controles-wrapper');
+  playerWrapper.append(divAudioControlsWrapper);
 
-  rangeVolume = document.createElement('input');
-  rangeVolume.classList.add('audio-range-volume');
-  rangeVolume.type = 'range';
-  rangeVolume.min = 0;
-  rangeVolume.max = 10;
-  rangeVolume.value = 5;
-  rangeVolume.addEventListener('change', (event) => {
-    console.log(+event.target.value);
-    audioSong.volume = +event.target.value / 10;
-  });
-  playerWrapper.append(rangeVolume);
+  divCurrentTime = document.createElement('div');
+  divAudioControlsWrapper.append(divCurrentTime);
 
   playBtn = document.createElement('div');
   playBtn.classList.add('play-btn');
@@ -60,7 +66,50 @@ export function drawPlayer(container, audio) {
     setBtnIcon();
     playpauseAudio();
   });
-  playerWrapper.append(playBtn);
+  divAudioControlsWrapper.append(playBtn);
+
+  let divVolumeWrapper = document.createElement('div');
+  divVolumeWrapper.classList.add('volume-wrapper');
+  divAudioControlsWrapper.append(divVolumeWrapper);
+
+  volumeBtnIcon = document.createElement('img');
+  volumeBtnIcon.addEventListener('click', () => {
+    if (rangeVolume.value == 0) {
+      volumeBtnIcon.src = volumePNG;
+      rangeVolume.value = 5;
+      audioSong.volume = 0.5;
+      rangeVolume.style.background =
+        'linear-gradient(to right,#ffffff ' + 50 + '%,#ffffff80 ' + 50 + '%)';
+    } else {
+      volumeBtnIcon.src = mutePNG;
+      rangeVolume.value = 0;
+      audioSong.volume = 0;
+      rangeVolume.style.background =
+        'linear-gradient(to right,#ffffff ' + 0 + '%,#ffffff80 ' + 0 + '%)';
+    }
+  });
+  volumeBtnIcon.src = volumePNG;
+  divVolumeWrapper.append(volumeBtnIcon);
+
+  rangeVolume = document.createElement('input');
+  rangeVolume.classList.add('audio-range-volume');
+  rangeVolume.type = 'range';
+  rangeVolume.min = 0;
+  rangeVolume.max = 10;
+  rangeVolume.addEventListener('change', (event) => {
+    +event.target.value === 0
+      ? (volumeBtnIcon.src = mutePNG)
+      : (volumeBtnIcon.src = volumePNG);
+
+    audioSong.volume = +event.target.value / 10;
+    rangeVolume.style.background =
+      'linear-gradient(to right,#ffffff ' +
+      +event.target.value * 10 +
+      '%,#ffffff80 ' +
+      +event.target.value * 10 +
+      '%)';
+  });
+  divVolumeWrapper.append(rangeVolume);
 
   playBtnIcon = document.createElement('img');
   playBtnIcon.classList.add('play-pause-icon');
@@ -121,8 +170,8 @@ function setCurrentDuration(number) {
 
 function setBtnIcon() {
   playBtnIcon.src = playBtn.classList.contains('play-mode')
-    ? pauseSVG
-    : playSVG;
+    ? pausePNG
+    : playPNG;
 }
 
 export function playpauseAudio() {
